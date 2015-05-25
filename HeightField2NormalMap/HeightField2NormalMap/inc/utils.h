d11a816342e47287e34fe4299e49b1169465b265
Graphics <softgiant@126.com>
2015-05-25 16:22:03 +0800

    a minor change.

--- HeightField2NormalMap/HeightField2NormalMap/inc/utils.h
+++ HeightField2NormalMap/HeightField2NormalMap/inc/utils.h
@@ -1,7 +1,15 @@
 #ifndef utils_h
 #define utils_h
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 wchar_t * char2wchar_t(const char *str);
 char *    wchar_t2char(const wchar_t *str);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
